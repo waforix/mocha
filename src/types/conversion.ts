7caf6c177852877bfabb5b3bc2edd0e2739a74dd
Madeline <madeline@firstage.gg>
2025-09-16 07:40:55 -0400

    Draft event dispatcher

--- src/types/conversion.ts
+++ src/types/conversion.ts
@@ -10,20 +10,6 @@ type SnakeCase<T extends string> =
     T extends `${infer U extends Lowercase<string>}${infer V extends Uppercase<string>}` ?
         `${U}_${SnakeCase<V>}` : T;
 
-
-/*
- * TAPI<T>, TLibrary<T>
- * 
- * Create a new type from an existing type, converting the
- * original type's properties from snake_case to camelCase
- * or vice-versa.
- * 
- * Usage:
- * type SomeLibraryType = TLibrary<SomeAPIType>;
- * type SomeAPIType = TAPI<SomeLibraryType>;
- * 
- */
-
 export type TAPI<T> = {
     [Property in keyof T as SnakeCase<Property & string>]: T[Property];
 }
